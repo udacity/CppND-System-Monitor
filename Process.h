eaebb19a619c5dc5553a8685fc4e7aee92414d70
haoyang-udacity <haoyang@udacity.com>
2019-04-24 13:54:54 -0700

    Update Process.h
    Removed extra characters

--- Process.h
+++ Process.h
@@ -44,7 +44,7 @@ string Process::getProcess(){
         return "";
     this->mem = ProcessParser::getVmSize(this->pid);
     this->upTime = ProcessParser::getProcUpTime(this->pid);
-    this->cpu = ProcessParser::getCpuPercent(this->pid); */
+    this->cpu = ProcessParser::getCpuPercent(this->pid);
 
     return (this->pid + "   " + //TODO: finish the string! this->user + "   "+ mem...cpu...upTime...;
 }
