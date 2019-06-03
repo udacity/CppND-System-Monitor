d04123c182e2caf8da389eea92828dcda4ea6ae0
Deep Doshi <deepdoshi@live.in>
2019-06-02 21:50:01 -0700

    fix: add class name to access function

--- SysInfo.h
+++ SysInfo.h
@@ -25,7 +25,7 @@ public:
     Initial data for individual cores is set
     System data is set
     */
-        this->getOtherCores(getNumberOfCores());
+        this->getOtherCores(ProcessParser::getNumberOfCores());
         this->setLastCpuMeasures();
         this->setAttributes();
         this-> OSname = ProcessParser::getOSName();
