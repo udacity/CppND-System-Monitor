bc65cc9eb93f30a8f7402823ebb9ec902800c1c9
truongconghiep <truongconghiep@gmail.com>
2019-08-03 08:56:28 +0200

    Create ProcessParser.h

---
+++ include/ProcessParser.h
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <iostream>
+#include <math.h>
+#include <thread>
+#include <chrono>
+#include <iterator>
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+#include <cerrno>
+#include <cstring>
+#include <dirent.h>
+#include <time.h>
+#include <unistd.h>
+#include <constants.h>
+
+class ProcessParser {
+    public:
+        static std::string getCmd(std::string pid);
+        static std::vector<std::string> getPidList();
+        static std::string getVmSize(std::string pid);
+        static std::string getCpuPercent(std::string pid);
+        static long int getSysUpTime();
+        static std::string getProcUpTime(std::string pid);
+        static std::string getProcUser(std::string pid);
+        static std::vector<std::string> getSysCpuPercent(std::string coreNumber = "");
+        static float getSysRamPercent();
+        static std::string getSysKernelVersion();
+        static int getTotalThreads();
+        static int getTotalNumberOfProcesses();
+        static int getNumberOfRunningProcesses();
+        static string getOsName();
+        static std::string printCpuStats(std::vector<std::string> values1, std::vector<std::string>values2);
+};
