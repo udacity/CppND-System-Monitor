6f23ea0049c32dcd864fd7be11b1d574ebb02747
truongconghiep <truongconghiep@gmail.com>
2019-08-03 10:46:47 +0200

    Create ProcessContainer.h

---
+++ include/ProcessContainer.h
@@ -0,0 +1,23 @@
+#ifndef PROCESS_CONTAINER_H
+#define PROCESS_CONTAINER_H
+
+#include "Process.h"
+#include <string>
+#include <vector>
+using std::string;
+using std::vector;
+
+class ProcessContainer {
+    public:
+        ProcessContainer()
+        {
+            this->refreshList();
+        }
+        void refreshList();
+        string printList();
+        vector<string> getList();
+
+    private:
+        vector<Process>_list;
+};
+#endif
