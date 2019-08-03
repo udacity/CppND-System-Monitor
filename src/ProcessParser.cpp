55f60d091a264bbe841f81b10787f46d98a7ba58
truongconghiep <truongconghiep@gmail.com>
2019-08-03 09:19:04 +0200

    add getCpuPercent

--- src/ProcessParser.cpp
+++ src/ProcessParser.cpp
@@ -24,3 +24,28 @@ string ProcessParser::getVmsSize(string pid)
     }
     return to_string(result);
 }
+
+string ProcessParser::getCpuPercent(string pid)
+{
+    string line;
+    string value;
+    float result;
+    ifstream stream = Util::getStream((Path::basePath()+ pid + "/" + Path::statPath()));
+    getline(stream, line);
+    string str = line;
+    istringstream buf(str);
+    istream_iterator<string> beg(buf), end;
+    vector<string> values(beg, end); // done!
+    // acquiring relevant times for calculation of active occupation of CPU for selected process
+    float utime = stof(ProcessParser::getProcUpTime(pid));
+    float stime = stof(values[14]);
+    float cutime = stof(values[15]);
+    float cstime = stof(values[16]);
+    float starttime = stof(values[21]);
+    float uptime = ProcessParser::getSysUpTime();
+    float freq = sysconf(_SC_CLK_TCK);
+    float total_time = utime + stime + cutime + cstime;
+    float seconds = uptime - (starttime/freq);
+    result = 100.0*((total_time/freq)/seconds);
+    return to_string(result);
+}
