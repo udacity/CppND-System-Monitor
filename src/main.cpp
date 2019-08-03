3c5304c2a4b81076aaa1d5693ffe1a60d1a70333
truongconghiep <truongconghiep@gmail.com>
2019-08-03 11:03:44 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,122 @@
 #include "ncurses_display.h"
 #include "system.h"
 
-int main() {
-  System system;
-  NCursesDisplay::Display(system);
+void writeSysInfoToConsole(SysInfo sys, WINDOW* sys_win)
+{
+    sys.setAttributes();
+
+    mvwprintw(sys_win,2,2,getCString(( "OS: " + sys.getOSName())));
+    mvwprintw(sys_win,3,2,getCString(( "Kernel version: " + sys.getKernelVersion())));
+    mvwprintw(sys_win,4,2,getCString( "CPU: "));
+    wattron(sys_win,COLOR_PAIR(1));
+    wprintw(sys_win,getCString(Util::getProgressBar(sys.getCpuPercent())));
+    wattroff(sys_win,COLOR_PAIR(1));
+    mvwprintw(sys_win,5,2,getCString(( "Other cores:")));
+    wattron(sys_win,COLOR_PAIR(1));
+    std::vector<std::string> val = sys.getCoresStats();
+    for (int i = 0; i < val.size(); i++) {
+        mvwprintw(sys_win,(6+i),2,getCString(val[i]));
+    }
+    wattroff(sys_win,COLOR_PAIR(1));
+    mvwprintw(sys_win,10,2,getCString(( "Memory: ")));
+    wattron(sys_win,COLOR_PAIR(1));
+    wprintw(sys_win,getCString(Util::getProgressBar(sys.getMemPercent())));
+    wattroff(sys_win,COLOR_PAIR(1));
+    mvwprintw(sys_win,11,2,getCString(( "Total Processes:" + sys.getTotalProc())));
+    mvwprintw(sys_win,12,2,getCString(( "Running Processes:" + sys.getRunningProc())));
+    mvwprintw(sys_win,13,2,getCString(( "Up Time: " + Util::convertToTime(sys.getUpTime()))));
+
+}
+
+void writeSysInfoToConsole(SysInfo sys, WINDOW* sys_win)
+{
+    sys.setAttributes();
+
+    mvwprintw(sys_win,2,2,getCString(( "OS: " + sys.getOSName())));
+    mvwprintw(sys_win,3,2,getCString(( "Kernel version: " + sys.getKernelVersion())));
+    mvwprintw(sys_win,4,2,getCString( "CPU: "));
+    wattron(sys_win,COLOR_PAIR(1));
+    wprintw(sys_win,getCString(Util::getProgressBar(sys.getCpuPercent())));
+    wattroff(sys_win,COLOR_PAIR(1));
+    mvwprintw(sys_win,5,2,getCString(( "Other cores:")));
+    wattron(sys_win,COLOR_PAIR(1));
+    std::vector<std::string> val = sys.getCoresStats();
+    for (int i = 0; i < val.size(); i++) {
+        mvwprintw(sys_win,(6+i),2,getCString(val[i]));
+    }
+    wattroff(sys_win,COLOR_PAIR(1));
+    mvwprintw(sys_win,10,2,getCString(( "Memory: ")));
+    wattron(sys_win,COLOR_PAIR(1));
+    wprintw(sys_win,getCString(Util::getProgressBar(sys.getMemPercent())));
+    wattroff(sys_win,COLOR_PAIR(1));
+    mvwprintw(sys_win,11,2,getCString(( "Total Processes:" + sys.getTotalProc())));
+    mvwprintw(sys_win,12,2,getCString(( "Running Processes:" + sys.getRunningProc())));
+    mvwprintw(sys_win,13,2,getCString(( "Up Time: " + Util::convertToTime(sys.getUpTime()))));
+
+}
+
+void writeSysInfoToConsole(SysInfo sys, WINDOW* sys_win)
+{
+    sys.setAttributes();
+
+    mvwprintw(sys_win,2,2,getCString(( "OS: " + sys.getOSName())));
+    mvwprintw(sys_win,3,2,getCString(( "Kernel version: " + sys.getKernelVersion())));
+    mvwprintw(sys_win,4,2,getCString( "CPU: "));
+    wattron(sys_win,COLOR_PAIR(1));
+    wprintw(sys_win,getCString(Util::getProgressBar(sys.getCpuPercent())));
+    wattroff(sys_win,COLOR_PAIR(1));
+    mvwprintw(sys_win,5,2,getCString(( "Other cores:")));
+    wattron(sys_win,COLOR_PAIR(1));
+    std::vector<std::string> val = sys.getCoresStats();
+    for (int i = 0; i < val.size(); i++) {
+        mvwprintw(sys_win,(6+i),2,getCString(val[i]));
+    }
+    wattroff(sys_win,COLOR_PAIR(1));
+    mvwprintw(sys_win,10,2,getCString(( "Memory: ")));
+    wattron(sys_win,COLOR_PAIR(1));
+    wprintw(sys_win,getCString(Util::getProgressBar(sys.getMemPercent())));
+    wattroff(sys_win,COLOR_PAIR(1));
+    mvwprintw(sys_win,11,2,getCString(( "Total Processes:" + sys.getTotalProc())));
+    mvwprintw(sys_win,12,2,getCString(( "Running Processes:" + sys.getRunningProc())));
+    mvwprintw(sys_win,13,2,getCString(( "Up Time: " + Util::convertToTime(sys.getUpTime()))));
+
+}
+
+void printMain(SysInfo sys,ProcessContainer procs)
+{
+    initscr(); // start curses mode
+    noecho(); // not printing input values
+    cbreak(); // Terminating on classic ctrl + c
+    startColor(); // Enabling color change of text
+    int yMax,xMax;
+    getmaxyx(stdscr,yMax,xMax); // getting size of window measured in lines and columns(column one char length)
+    WINDOW *sys_win = newwin(17,xMax-1,0,0);
+    WINDOW *proc_win = newwin(15,xMax-1,18,0);
+
+    init_pair(1,COLOR_BLUE,COLOR_BLACK);
+    init_pair(2,COLOR_GREEN,COLOR_BLACK);
+
+    while (true) {
+        box(sys_win,0,0);
+        box (proc_win,0,0);
+        writeSysInfoToConsole(sys,sys_win);
+        getProcessListToConsole(procs,proc_win);
+        wrefresh(sys_win);
+        wrefresh(proc_win);
+        refresh();
+        sleep(1);
+    }
+    endwin();
+}
+
+
+int main( int   argc, char *argv[] )
+{
+     //Object which contains list of current processes, Container for Process Class
+    ProcessContainer procs;
+    // Object which containts relevant methods and attributes regarding system details
+    SysInfo sys;
+    //string s = writeToConsole(sys);
+    printMain(sys,procs);
+    return 0;
 }
