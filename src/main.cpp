00fd1d4f4dc0824535301e69c65d0f7c6a1cf9ce
truongconghiep <truongconghiep@gmail.com>
2019-10-06 17:35:17 +0200

    remove SysInfo

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include "ProcessContainer.h"
-#include "SysInfo.h"
 #include "ncurses.h"
 #include "ncurses_display.h"
 #include "system.h"
@@ -9,35 +8,6 @@ char *getCString(std::string str) {
   char *cstr = new char[str.length() + 1];
   std::strcpy(cstr, str.c_str());
   return cstr;
-}
-
-void writeSysInfoToConsole(SysInfo sys, WINDOW *sys_win) {
-  sys.setAttributes();
-
-  mvwprintw(sys_win, 2, 2, getCString(("OS: " + sys.getOsName())));
-  mvwprintw(sys_win, 3, 2,
-            getCString(("Kernel version: " + sys.getKernelVersion())));
-  mvwprintw(sys_win, 4, 2, getCString("CPU: "));
-  wattron(sys_win, COLOR_PAIR(1));
-  wprintw(sys_win, getCString(Util::getProgressBar(sys.getCpuPercent())));
-  wattroff(sys_win, COLOR_PAIR(1));
-  mvwprintw(sys_win, 5, 2, getCString(("Other cores:")));
-  wattron(sys_win, COLOR_PAIR(1));
-  std::vector<std::string> val = sys.getCoresStats();
-  for (unsigned int i = 0; i < val.size(); i++) {
-    mvwprintw(sys_win, (6 + i), 2, getCString(val[i]));
-  }
-  wattroff(sys_win, COLOR_PAIR(1));
-  mvwprintw(sys_win, 10, 2, getCString(("Memory: ")));
-  wattron(sys_win, COLOR_PAIR(1));
-  wprintw(sys_win, getCString(Util::getProgressBar(sys.getMemPercent())));
-  wattroff(sys_win, COLOR_PAIR(1));
-  mvwprintw(sys_win, 11, 2,
-            getCString(("Total Processes:" + sys.getTotalProc())));
-  mvwprintw(sys_win, 12, 2,
-            getCString(("Running Processes:" + sys.getRunningProc())));
-  mvwprintw(sys_win, 13, 2,
-            getCString(("Up Time: " + Util::convertToTime(sys.getUpTime()))));
 }
 
 void writeSysInfoToConsole1(System sys, WINDOW *sys_win) {
