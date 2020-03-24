869da6ddadd142e5749cbfb875c5c2c9ce1b0a27
zh2eou <zh2eou@gmail.com>
2020-03-23 20:48:17 -0400

    Update format.cpp
    implement ElapsedTime

--- src/format.cpp
+++ src/format.cpp
@@ -8,4 +8,13 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long times) { 
+    long hours = times / 60 / 60;
+    long minutes = times / 60 % 60;
+    long seconds = times % 60;
+    // There's probably better way to do the 0 padding
+    string hh = (hours < 10 ? "0" : "") + std::to_string(hours);
+    string mm = (minutes < 10 ? "0" : "") + std::to_string(minutes);
+    string ss = (seconds < 10 ? "0" : "") + std::to_string(seconds);
+    return hh + ":" + mm + ":" + ss;
+}
