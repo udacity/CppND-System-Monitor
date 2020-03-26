5c599ef48433f33fad1a6e20cc79945028bdbef9
zh2eou <zh2eou@gmail.com>
2020-03-25 21:36:41 -0400

    Update system.cpp
    Revert changes, fix typo for RunningProcesses

--- src/system.cpp
+++ src/system.cpp
@@ -15,10 +15,10 @@ using std::string;
 using std::vector;
 
 // TODO: Return the system's CPU
-Processor& System::Cpu() { return LinuxParser::CpuUtilization(); }
+Processor& System::Cpu() { return cpu_; }
 
 // TODO: Return a container composed of the system's processes
-vector<Process>& System::Processes() { return LinuxParser::Pids(); }
+vector<Process>& System::Processes() { return processes_; }
 
 // TODO: Return the system's kernel identifier (string)
 std::string System::Kernel() { return LinuxParser::Kernel(); }
@@ -30,7 +30,7 @@ float System::MemoryUtilization() { return LinuxParser::MemoryUtilization(); }
 std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
 
 // TODO: Return the number of processes actively running on the system
-int System::RunningProcesses() { return LinuxParser::TotalProcesses(); }
+int System::RunningProcesses() { return LinuxParser::RunningProcesses(); }
 
 // TODO: Return the total number of processes on the system
 int System::TotalProcesses() { return LinuxParser::TotalProcesses(); }
