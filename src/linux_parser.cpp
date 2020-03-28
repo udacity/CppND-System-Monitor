57874bb29b0ae8be16f86407c28d7df38e994dc6
zh2eou <zh2eou@gmail.com>
2020-03-27 22:00:55 -0400

    Update linux_parser.cpp
    Implement long UpTime()

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -101,8 +101,19 @@ float LinuxParser::MemoryUtilization() {
   return memUtilization / stof(memTotal); 
 }
 
-// TODO: Read and return the system uptime
-long LinuxParser::UpTime() { return 0; }
+// Read and return the system uptime
+long LinuxParser::UpTime() { 
+  string upTime, idleTime;
+  string line;
+  std::ifstream stream(kProcDirectory + kUptimeFilename);
+  if (stream.is_open()) {
+    std::getline(stream, line);
+    std::istringstream linestream(line);
+    linestream >> upTime >> idleTime;
+  }
+  
+  return stol(upTime);
+ }
 
 // TODO: Read and return the number of jiffies for the system
 long LinuxParser::Jiffies() { return 0; }
