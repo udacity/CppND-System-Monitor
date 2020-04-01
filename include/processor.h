cf3e4d586d7027ed16e3e88c90cbfc01f2bc71c8
Mattia De Prisco <mattia.dipi@gmail.com>
2020-04-01 15:23:20 +0200

    Add constructor for processor from string of values

--- include/processor.h
+++ include/processor.h
@@ -1,40 +1,29 @@
 #ifndef PROCESSOR_H
 #define PROCESSOR_H
 
-#include <functional>
 #include <string>
-#include <vector>
 
 class Processor
 {
  public:
-  float Utilization();  // TODO: See src/processor.cpp
-
   Processor() = default;
 
-  Processor(const std::vector<std::string>& v)
-  {
-    if (v.size() != 10) return;
-    std::vector<std::reference_wrapper<long>> values{
-        user, nice,    system, idle,  iowait,
-        irq,  softirq, steal,  guest, guest_nice};
-    for (size_t i = 0; i < v.size(); i++)
-    {
-      values.at(i).get() = std::stoi(v.at(i));
-    }
-  }
+  Processor(const std::string& values);
 
-  // private:
-  long user{};
-  long nice{};
-  long system{};
-  long idle{};
-  long iowait{};
-  long irq{};
-  long softirq{};
-  long steal{};
-  long guest{};
-  long guest_nice{};
+  float Utilization();  // TODO: See src/processor.cpp
+
+ private:
+  std::string cpu_name_{};
+  long user_{};
+  long nice_{};
+  long system_{};
+  long idle_{};
+  long iowait_{};
+  long irq_{};
+  long softirq_{};
+  long steal_{};
+  long guest_{};
+  long guest_nice_{};
 };
 
 #endif
--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,12 @@
 #include "processor.h"
+#include <sstream>
+
+Processor::Processor(const std::string& values)
+{
+  std::stringstream ss{values};
+  ss >> cpu_name_ >> user_ >> nice_ >> system_ >> idle_ >> iowait_ >> irq_ >>
+      softirq_ >> steal_ >> guest_ >> guest_nice_;
+}
 
 // TODO: Return the aggregate CPU utilization
 float Processor::Utilization() { return 0.0; }
