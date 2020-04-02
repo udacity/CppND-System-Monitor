d8350ef424454671f47adb2491b4ca468232b0ab
Mattia De Prisco <mattia.dipi@gmail.com>
2020-04-02 13:19:54 +0200

    Update main with processor for system

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include "linux_parser.h"
 #include "ncurses_display.h"
 #include "system.h"
 
 int main()
 {
-  System system;
+  Processor p{LinuxParser::CpusUtilization().front()};
+  System system(p);
   NCursesDisplay::Display(system);
 }
