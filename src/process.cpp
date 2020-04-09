ab28fb35942881cd4a1607c5c1f73b3a624a6733
zh2eou <zh2eou@gmail.com>
2020-04-08 20:55:26 -0400

    Stub out problematic Process::Ram (and LinuxParse::Ram)  which is causing local system monitor to hang

--- src/process.cpp
+++ src/process.cpp
@@ -33,7 +33,10 @@ float Process::CpuUtilization() {
 string Process::Command() { return LinuxParser::Command(Pid()); }
 
 // Return this process's memory utilization
-string Process::Ram() { return LinuxParser::Ram(Pid()); }
+string Process::Ram() { 
+    // return LinuxParser::Ram(Pid()); 
+    return "nil";
+}
 
 // Return the user (name) that generated this process
 string Process::User() { return LinuxParser::User(Pid()); }
