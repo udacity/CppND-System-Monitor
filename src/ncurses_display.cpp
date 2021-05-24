7dd0c631cff0db266d804e1d01565b9c50520c9e
Shing-Yan Loo <yan99033@gmail.com>
2021-05-24 15:03:04 -0600

    Update ncurses_display.cpp
    Related issue: https://knowledge.udacity.com/questions/160777

--- src/ncurses_display.cpp
+++ src/ncurses_display.cpp
@@ -70,7 +70,10 @@ void NCursesDisplay::DisplayProcesses(std::vector<Process>& processes,
   mvwprintw(window, row, command_column, "COMMAND");
   wattroff(window, COLOR_PAIR(2));
   for (int i = 0; i < n; ++i) {
-    mvwprintw(window, ++row, pid_column, to_string(processes[i].Pid()).c_str());
+    // Clear the line
+    mvwprintw(window, ++row, pid_column, (string(window->_maxx-2, ' ').c_str()));
+    
+    mvwprintw(window, row, pid_column, to_string(processes[i].Pid()).c_str());
     mvwprintw(window, row, user_column, processes[i].User().c_str());
     float cpu = processes[i].CpuUtilization() * 100;
     mvwprintw(window, row, cpu_column, to_string(cpu).substr(0, 4).c_str());
