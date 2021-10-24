694f779eaf741f06b6a673a26aff7527e6cf2372
UAnjali <89843167+UAnjali@users.noreply.github.com>
2021-10-25 00:13:37 +0530

    Update system.cpp

--- src/system.cpp
+++ src/system.cpp
@@ -12,6 +12,9 @@ using std::set;
 using std::size_t;
 using std::string;
 using std::vector;
+/*You need to complete the mentioned TODOs in order to satisfy the rubric criteria "The student will be able to extract and display basic data about the system."
+
+You need to properly format the uptime. Refer to the comments mentioned in format. cpp for formatting the uptime.*/
 
 // TODO: Return the system's CPU
 Processor& System::Cpu() { return cpu_; }
