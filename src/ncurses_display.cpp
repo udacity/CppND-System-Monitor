9f0948e12be68fe0352f902d5ec88aca2ca490c4
UAnjali <89843167+UAnjali@users.noreply.github.com>
2021-10-25 00:14:46 +0530

    Update ncurses_display.cpp

--- src/ncurses_display.cpp
+++ src/ncurses_display.cpp
@@ -70,6 +70,7 @@ void NCursesDisplay::DisplayProcesses(std::vector<Process>& processes,
   mvwprintw(window, row, command_column, "COMMAND");
   wattroff(window, COLOR_PAIR(2));
   for (int i = 0; i < n; ++i) {
+    //You need to take care of the fact that the cpu utilization has already been multiplied by 100.
     // Clear the line
     mvwprintw(window, ++row, pid_column, (string(window->_maxx-2, ' ').c_str()));
     
