2029a36a4485afb54a1a357c4979bd79dbd4b952
Rutvik Patel <rutvik.p4t3l@gmail.com>
2022-08-21 01:38:28 +0530

    remove completed TODOs

--- include/system.h
+++ include/system.h
@@ -9,16 +9,16 @@
 
 class System {
  public:
-  Processor& Cpu();                   // TODO: See src/system.cpp
+  Processor& Cpu();                   // See src/system.cpp
   std::vector<Process>& Processes();  // TODO: See src/system.cpp
-  float MemoryUtilization();          // TODO: See src/system.cpp
-  long UpTime();                      // TODO: See src/system.cpp
-  int TotalProcesses();               // TODO: See src/system.cpp
-  int RunningProcesses();             // TODO: See src/system.cpp
-  std::string Kernel();               // TODO: See src/system.cpp
-  std::string OperatingSystem();      // TODO: See src/system.cpp
+  float MemoryUtilization();          // See src/system.cpp
+  long UpTime();                      // See src/system.cpp
+  int TotalProcesses();               // See src/system.cpp
+  int RunningProcesses();             // See src/system.cpp
+  std::string Kernel();               // See src/system.cpp
+  std::string OperatingSystem();      // See src/system.cpp
 
-  // TODO: Define any necessary private members
+  // Define any necessary private members
  private:
   Processor cpu_ = {};
   std::vector<Process> processes_ = {};
