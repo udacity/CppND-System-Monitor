89e2b215701f1063084cad6b65d036da9fef2556
Rutvik Patel <rutvik.p4t3l@gmail.com>
2022-08-20 22:02:47 +0530

    return os name

--- src/system.cpp
+++ src/system.cpp
@@ -7,6 +7,7 @@
 #include "process.h"
 #include "processor.h"
 #include "system.h"
+#include "linux_parser.h"
 
 using std::set;
 using std::size_t;
@@ -29,7 +30,7 @@ std::string System::Kernel() { return string(); }
 float System::MemoryUtilization() { return 0.0; }
 
 // TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
 
 // TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { return 0; }
